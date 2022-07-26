92ef56e120ca5e639b4fa7ed1501353f13dc0f6a
WizMe-M <timkin.moxim@mail.ru>
2022-07-26 11:38:08 +0300

    styled control
    - added property TextForeground to text
    - replaced content with template
    - binded panel background and text foreground to UC properties

--- ClickablePanel.axaml.cs
+++ ClickablePanel.axaml.cs
@@ -1,17 +1,34 @@
-﻿using Avalonia.Controls;
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Controls.Primitives;
 using Avalonia.Markup.Xaml;
+using Avalonia.Media;
 
 namespace ControlDevelopLearning;
 
 public partial class ClickablePanel : UserControl
 {
+    private Panel _rootPanel;
+    
     public ClickablePanel()
     {
         InitializeComponent();
     }
 
-    private void InitializeComponent()
+    private void InitializeComponent() => AvaloniaXamlLoader.Load(this);
+
+    public static readonly StyledProperty<IBrush> TextForegroundProperty =
+        AvaloniaProperty.Register<ClickablePanel, IBrush>(nameof(TextForeground), Brushes.Black, true);
+
+    public IBrush TextForeground
     {
-        AvaloniaXamlLoader.Load(this);
+        get => GetValue(TextForegroundProperty);
+        set => SetValue(TextForegroundProperty, value);
+    }
+
+    protected override void OnApplyTemplate(TemplateAppliedEventArgs e)
+    {
+        base.OnApplyTemplate(e);
+        _rootPanel = this.FindControl<Panel>("PART_RootPanel");
     }
 }
