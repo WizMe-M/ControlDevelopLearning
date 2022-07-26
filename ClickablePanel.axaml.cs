75975e6b8cd4346fe995e7960e74ae05c0a8c824
WizMe-M <timkin.moxim@mail.ru>
2022-07-26 11:14:18 +0300

    added control ClickablePanel

---
+++ ClickablePanel.axaml.cs
@@ -0,0 +1,17 @@
+﻿using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace ControlDevelopLearning;
+
+public partial class ClickablePanel : UserControl
+{
+    public ClickablePanel()
+    {
+        InitializeComponent();
+    }
+
+    private void InitializeComponent()
+    {
+        AvaloniaXamlLoader.Load(this);
+    }
+}
